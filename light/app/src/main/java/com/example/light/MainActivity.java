de49b9d9ea9609921c35fe2e348928c7ea8a1acb
Dante <jazzy.d1302@outlook.com>
2020-03-21 12:14:09 -0400

    bulid

--- light/app/src/main/java/com/example/light/MainActivity.java
+++ light/app/src/main/java/com/example/light/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.light;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final TextView main = findViewById(R.id.Main);
+
+        final Button dante = findViewById(R.id.dante);
+        dante.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                main.setText("Hello Dante");
+            }
+        });
+
+        final Button evan = findViewById(R.id.evan);
+        evan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                main.setText("Hello Evan");
+            }
+        });
     }
 }
