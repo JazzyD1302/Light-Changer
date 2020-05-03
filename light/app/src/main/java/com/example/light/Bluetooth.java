67810ba5601c5a611b15182bb25efd161d3e59ca
Dante <jazzy.d1302@outlook.com>
2020-05-03 10:02:29 -0400

    fixed crashing

---
+++ light/app/src/main/java/com/example/light/Bluetooth.java
@@ -0,0 +1,85 @@
+package com.example.light;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+public class Bluetooth {
+    static BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+
+    static UUID uuid = null;
+
+    public Bluetooth() throws IOException {
+    }
+
+    public static void main(String[] args){
+
+
+
+        if(!adapter.isEnabled()){
+            System.out.println("Please enble bluetooth");
+        }else{
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+
+// If there’s 1 or more paired devices...//
+        if (pairedDevices.size() > 0) {
+
+            //...then loop through these devices//
+            for (BluetoothDevice device : pairedDevices) {
+                //Retrieve each device’s public identifier and MAC address. Add each device’s name and address to an ArrayAdapter, ready to incorporate into a
+                //ListView
+                ArrayList<String> mArrayAdapter = null;
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        }
+    }
+
+    public static void createUUID(){
+        uuid = UUID.fromString("20074835-894a-42b0-a0aa-d54f59e97a5a");
+    }
+
+
+    public static BluetoothServerSocket blueServersocket;
+    static BluetoothDevice bluetoothDevice;
+    private static BluetoothSocket bluesocket;
+    static void socketsRecord(){
+        try {
+            bluesocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            blueServersocket = adapter.listenUsingRfcommWithServiceRecord("Light", uuid);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ;
+    private InputStream in;
+    private OutputStream out;
+
+    public static void send(int color) throws IOException {
+
+        InputStream tempIn = color;
+        OutputStream tempOut = null;
+//        try {
+//            tempIn = bluesocket.getInputStream();
+//            tempOut = bluesocket.getOutputStream();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+
+}
--- light/app/src/main/java/com/example/light/MainActivity.java
+++ light/app/src/main/java/com/example/light/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.light;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHeadset;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.BluetoothSocket;
@@ -14,15 +15,20 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        Bluetooth.createUUID();
+//        Bluetooth.main(null);
 
         final TextView colour = findViewById(R.id.Color);
 
@@ -31,19 +37,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 colour.setBackgroundResource(R.color.Red);
                 try {
-                    bluetooth(null, 1);
+                    Bluetooth.send( 1);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
-
         final Button blue = findViewById(R.id.Blue);
         blue.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 colour.setBackgroundResource(R.color.Blue);
                 try {
-                    bluetooth(null, 2);
+                    Bluetooth.send( 2);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -55,30 +60,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 colour.setBackgroundResource(R.color.Green);
                 try {
-                    bluetooth(null, 3);
+                    Bluetooth.send(3);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
 
-    }
-
-    public BluetoothSocket bluesocket;
-    private InputStream in;
-    private OutputStream out;
-
-    public void bluetooth(BluetoothSocket socket, int color) throws IOException {
-        bluesocket=socket;
-        InputStream tempIn = null;
-        OutputStream tempOut = null;
-        try {
-            tempIn = bluesocket.getInputStream();
-            tempOut = bluesocket.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        out.write(color);
     }
 }
